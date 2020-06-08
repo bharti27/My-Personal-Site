c246959cb2be46d4b7611e9d80fc32eafd3a14c3
bharti27 <bharti2792@gmail.com>
2020-06-08 15:00:06 -0400

    fixed

--- src/AboutComponent.js
+++ src/AboutComponent.js
@@ -11,7 +11,7 @@ export default class AboutComponent extends React.Component {
                     <section>
                 <p className={"about-para"}>This is a place where I think I should brag about myself. So, here it goes. </p>
                 <p className={"about-para"}>After tirelessly working on various projects and achieving several milestones I moved to U.S in 2018 to pursue my dream. After two years of sleep-deprived long nights
-                    and a lot of coffee, I successfully completed my Masters's degree in Software Engineering from George Mason University. And now I am still sleep-deprived and running on coffee but... </p>
+                    and a lot of coffee, I successfully completed my Master's degree in Software Engineering from George Mason University. And now I am still sleep-deprived and running on coffee but... </p>
 
                 <p className={"about-para no-justify"}>I really Love to <Typical
                     steps={['#Code!', 1500, '#Create!', 1500, '#Innovate!', 1500 ]}
