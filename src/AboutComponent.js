b2351cbbcaff64ad791a91fdd727f8313d3ab0d7
bharti27 <bharti2792@gmail.com>
2020-06-08 14:42:43 -0400

    fixed

--- src/AboutComponent.js
+++ src/AboutComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Typical from 'react-typical'
 import Anime from "react-anime";
-
+//  <p className={"about-para"}>What, this is enough but wait one last thing...</p>
 export default class AboutComponent extends React.Component {
     render () {
         return (
@@ -12,7 +12,7 @@ export default class AboutComponent extends React.Component {
                 <p className={"about-para"}>This is a place where I think I should brag about myself. So, here it goes. </p>
                 <p className={"about-para"}>After tirelessly working on various projects and achieving several milestones I moved to U.S in 2018 to pursue my dream. After two years of sleep-deprived long nights
                     and a lot of coffee, I successfully completed my Masters's degree in Software Engineering from George Mason University. And now I am still sleep-deprived and running on coffee but... </p>
-                <p className={"about-para"}>What, this is enough but wait one last thing...</p>
+
                 <p className={"about-para no-justify"}>I really Love to <Typical
                     steps={['#Code!', 1500, '#Create!', 1500, '#Innovate!', 1500 ]}
                     loop={Infinity}
